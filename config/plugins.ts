88fdb186a5ce0f1629f7df0a1f1c78bfde68cc51
og developers <developers506@gmail.com>
2025-06-24 14:45:57 +0530

    S3 configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        baseUrl: process.env.CDN_BASE_URL as string,
+        s3Options: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+          region: process.env.AWS_REGION as string,
+          params: {
+            Bucket: process.env.AWS_BUCKET_NAME as string,
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
