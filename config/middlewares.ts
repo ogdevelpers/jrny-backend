36d2e1c09fae58159cda70320f6cc869800c0b30
og developers <developers506@gmail.com>
2025-06-26 21:37:17 +0530

    Added config changes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,32 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "https://d2ha2390ib21tl.cloudfront.net",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "https://d2ha2390ib21tl.cloudfront.net",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
